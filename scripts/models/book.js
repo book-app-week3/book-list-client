0c527fddd97e42d025c81605121499a49f1df868
jjkusowski <jjkusowski@yahoo.com>
2017-11-08 13:39:36 -0800

    MVP day 1.  README updated for submit.

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -4,6 +4,11 @@ var app = app || {};
 var __API_URL__ = 'https://gj-jk-booklist.herokuapp.com';
 
 (function(module) {
+  function errorCallback(err) {
+    console.log(err);
+    module.errorView.initErrorPage(err);
+  }
+
   function Book (rawBookObj) {
     Object.keys(rawBookObj).forEach(key => this[key] = rawBookObj[key]);
   }
@@ -19,8 +24,8 @@ var __API_URL__ = 'https://gj-jk-booklist.herokuapp.com';
   Book.fetchAll = callBack =>
     $.get(`${__API_URL__}/api/v1/books`)
       .then(Book.loadAll)
-      .then(callBack);
-  //.then(errorCallBack);
+      .then(callBack)
+      .then(errorCallback);
   //   Book.createBook = book =>
   //     $.post(`${__API_URL__}/books/add`, book)
   //       .then(() => page('/'));
