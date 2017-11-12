118c23a78925fdb8e22249f21a3c0a8f7220fbf5
William Johnson <williamgjohnson@live.com>
2017-11-12 14:55:42 -0800

    gh-pages test routes 1

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -9,5 +9,5 @@ page('/', ctx => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/new', ctx => app.bookView.initAddForm(ctx));
 page('/books/:book_id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
 page('/books/:book_id/update', ctx => app.Book.fetchOne(ctx, app.bookView.initUpdateFormPage));
-page('/admin', () => app.adminView.initAdminPage());
+page('./admin', () => app.adminView.initAdminPage());
 page();
